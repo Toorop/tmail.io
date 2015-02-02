6cb58243f7c05c06e1477d26e0e9a33cd872ab0f
Stéphane Depierrepont aka Toorop <toorop@toorop.fr>
2015-02-02 16:30:02 +0100

    Doc installer

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # generate
-hugo --theme=hyde --buildDrafts;
+#hugo --theme=hyde --buildDrafts;
 
 # send everything in public to the root of blog
 rsync -avz public/* root@tmail.io:/var/www/tmail.io/
