cb11598d19f04c5827fe3b4411fe445cc7794487
Stéphane Depierrepont <toorop@gmail.com>
2022-11-23 10:48:08 +0100

    change hostname

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 hugo --theme=tmail;
 
 # send everything in public to the root of blog
-rsync -avz --del public/* root@tmail.io:/var/www/tmail.io/
+rsync -avz --del public/* root@tmail.dpp.st:/var/www/tmail.dpp.st/
